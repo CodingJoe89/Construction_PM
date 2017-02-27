c01971ddb92683eb7a428da92dfb14e3f04c87ab
Avs-cd-bc <Avs.cd.bc@gmail.com>
2017-02-27 13:40:36 -0800

    Adding login controller/partial, adding admin project and tasks controllers/partials

---
+++ client/controllers/loginRegistrationController.js
@@ -0,0 +1,77 @@
+angular.module("app").controller("LoginRegistrationController", function($location){
+  var self = this;
+  self.specialtyOptions = ["Structural", "Mechanical", "Electrical", "Security", "Finishings"];
+  self.newCompany = {specialty: []};
+  self.loginErrorFlag;
+  self.registrationErrorFlag;
+  self.companyRegistrationErrorFlag;
+  self.error;
+
+  //helper function to reset all error flags
+  function resetFlags(){
+    self.loginErrorFlag = false;
+    self.registrationErrorFlag =  false;
+    self.companyRegistrationErrorFlag = false;
+  }
+  resetFlags();
+
+  // need factory methods
+  self.login = function(){
+    resetFlags();
+    if(self.loginName && self.loginPassword){
+      var loginCredentials = {
+        name: self.loginName,
+        password: self.loginPassword};
+        console.log(loginCredentials);
+      }
+      else{
+        self.error = "Login Error";
+        self.loginErrorFlag = true;
+      }
+    };
+
+    //need factory methods
+  self.register = function(){
+    resetFlags();
+    if(self.registrationName && self.registrationPassword &&  self.registrationPassword == self.registrationPasswordConfirm){
+      var newUser = {
+        name: self.registrationName,
+        email: self.registrationEmail,
+        password: self.registrationPassword};
+        //use factory methods to post the newUser to the backend
+        console.log(newUser);
+      }
+      else{
+        self.registrationErrorFlag = true;
+        self.error = "Registration Error!";
+        console.log("Registration Error");
+      }
+    };
+
+  // need factory methods
+  self.addSpecialty = function(specialty){
+    if(self.newCompany.specialty.indexOf(specialty) == -1){
+      self.newCompany.specialty.push(specialty);
+    }
+    else{
+      self.newCompany.specialty.splice(self.newCompany.specialty.indexOf(specialty), 1);
+    }
+    console.log(self.newCompany.specialty);
+  };
+
+  // need factory methods
+  self.registerCompany = function(){
+    resetFlags();
+    if(self.companyName){
+      self.newCompany.name = self.companyName;
+      if(typeof(self.specialtyOther) == "string"){
+        self.newCompany.specialty.push(self.specialtyOther);
+      }
+      console.log(self.newCompany);
+    }
+    else{
+      self.companyRegistrationErrorFlag = true;
+      self.error = "Company Registration Error";
+    }
+  };
+});
